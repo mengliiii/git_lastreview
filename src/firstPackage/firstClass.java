f066f0606b344a5f544887d8611585271cd3849f
mengliiii <menglivy@hotmail.com>
2023-01-07 14:29:57 -0500

    edit two class file

--- src/firstPackage/firstClass.java
+++ src/firstPackage/firstClass.java
@@ -2,5 +2,7 @@ package firstPackage;
 
 public class firstClass {
 
-
+    public static void main(String[] args) {
+        System.out.println("First");
+    }
 }
--- src/firstPackage/secondClass.java
+++ src/firstPackage/secondClass.java
@@ -1,4 +1,8 @@
 package firstPackage;
 
 public class secondClass {
+
+    public static void main(String[] args) {
+        System.out.println("Second");
+    }
 }
